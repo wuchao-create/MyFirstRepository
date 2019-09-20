54b2bfd9ea8d848ede753d28cb3492d89a775336
wuchao-create <1079755637@qq.com>
2019-09-20 20:05:06 +0800

    wuchao commit

---
+++ asdf.java
@@ -0,0 +1,9 @@
+public class asdf(){
+	public static void main(String[] args){
+				system.out.println("zhang pan is a shadiao");
+		system.out.println("wang qiao is also a shadiao");
+		system.out.println("oh you are a hamapi");
+		system.out.println("oh you are a guapi");
+		system.out.println("oh you are a caigouzi");
+	}
+}
